bdecd729099cadf79acbb97c40ffc1cc3bcfe205
sabrin99m <93140098+sabrin99m@users.noreply.github.com>
2023-03-21 15:23:25 +0100

    inizio

--- DhackFS/main.py
+++ DhackFS/main.py
@@ -1,16 +1,30 @@
-from winfspy import (
-    FileSystem,
-    BaseFileSystemOperations,
-    enable_debug_log,
-    FILE_ATTRIBUTE,
-    CREATE_FILE_CREATE_OPTIONS,
-    NTStatusObjectNameNotFound,
-    NTStatusDirectoryNotEmpty,
-    NTStatusNotADirectory,
-    NTStatusObjectNameCollision,
-    NTStatusAccessDenied,
-    NTStatusEndOfFile,
-)
+import winfspy
 
 
+class MyFileSystem(winfspy.FileSystem):
+    def __init__(self):
+        super().__init__()
 
+    def on_read_directory(self, path: str):
+        # Implement the logic for reading a directory here
+        pass
+
+    def on_get_file_info(self, path: str, fh=None):
+        # Implement the logic for getting file info here
+        pass
+
+    def on_create(self, path: str, flags, attrs):
+        # Implement the logic for creating a file or directory here
+        pass
+
+    def on_open(self, path: str, flags):
+        # Implement the logic for opening a file here
+        pass
+
+    def on_read(self, path: str, offset, length):
+        # Implement the logic for reading a file here
+        pass
+
+    def on_write(self, path: str, offset, data):
+        # Implement the logic for writing
+        pass
