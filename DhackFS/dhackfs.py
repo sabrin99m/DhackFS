a2cb13fb1815d918d33e6fc6bf45753d5cc2901d
sabrin99m <93140098+sabrin99m@users.noreply.github.com>
2023-04-18 17:39:51 +0200

    update operazioni

--- DhackFS/dhackfs.py
+++ DhackFS/dhackfs.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from pathlib import Path, PureWindowsPath
 
 from winfspy import (
@@ -17,11 +18,14 @@ from winfspy import (
 
 def creaFS (mountpoint):                   #il file system viene creato
     mountpoint = Path(mountpoint)
-   
-    pass
+    vfs=FileSystem.__init__(mountpoint)
+    return vfs
 
-def main(mountpoint="X:"):                  #nel main il file system viene avviato e stoppato 
-    vfs=creaFS(mountpoint) 
+def main():                               #nel main il file system viene avviato e stoppato 
+    parser=argparse.ArgumentParser()      
+    parser.add_argument("mountpoint")      
+    args=parser.parse_args
+    vfs=creaFS(args.mountpoint) 
     vfs.start()                             #importato da file_system.py di winfspy
     print("VirtualFS started")
     quit=input("Want to quit? Y/N")
